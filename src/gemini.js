23e5060b7dfa2099ae6559493ab5c7388481eab1
Alishkoo <baisholanov.alibek@gmail.com>
2024-08-29 01:36:17 +0500

    tried to activate

--- src/gemini.js
+++ src/gemini.js
@@ -1,38 +1,47 @@
+import "pdf-parse";
 import { GoogleGenerativeAI } from "@google/generative-ai";
+import {PDFLoader} from "@langchain/community/document_loaders/fs/pdf";
+import { FaissStore } from "langchain/vectorstores/faiss";
+import { RecursiveCharacterTextSplitter} from "langchain/text_splitter"
 import dotenv from "dotenv";
 
 dotenv.config();
-
-// async function run() {
-// // For embeddings, use the Text Embeddings model
-// const model = genAI.getGenerativeModel({ model: "text-embedding-004"});
-
-// const text = "The quick brown fox jumps over the lazy dog."
-
-// const result = await model.embedContent(text);
-// const embedding = result.embedding;
-// console.log(embedding.values);
-// }
-
-// run();
 
 class GeminiAI {
     constructor(){}
 
     async emdedding(text){
         try{    
+            const loader = new PDFLoader("../pdf/pdf_example.pdf");
+            const docs = await loader.load();
+            console.log("docs loaded")
+
+            const textSplitter = new RecursiveCharacterTextSplitter({
+                chuckSize: 1000,
+                chunkOverlap: 200,
+            });
+
             const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
-            const model = genAI.getGenerativeModel({ model: "text-embedding-004"});
+            const embeddings = genAI.getGenerativeModel({ model: "text-embedding-004"});
 
-            const result = await model.embedContent(text);
-            const embedding = result.embedding;
-            return embedding.values;
+            const docsOutput = await textSplitter.splitDocuments(docs);
+            let vectorStore = new FaissStore.fromDocuments(
+                docsOutput, 
+                embeddings, 
+            );
+            console.log("vector store created")
+            await vectorStore.save("../faiss_store");
+
+            // const result = await model.embedContent(text);
+            // const embedding = result.embedding;
+            // return embedding.values;
 
         }catch(e){
             console.log("Error with embedding",e.message);
         }
     }
+
 }
 
 export const gemini = new GeminiAI();
